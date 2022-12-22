9b2cbe3c0829c1f4dc0e60491eb06ec46a37e063
clacson95 <caleb.lacson@gmail.com>
2022-12-21 20:05:13 -0500

    troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require("mongoose");
 
 // wrap Mongoose around local connection to MongoDB
 const connectionString =
-  process.env.MONGODB_URI || "mongodb://localhost/socialNetworkApi";
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialApiDB';
 
 connect(connectionString, {
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const db = require("./config/connection");
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 db.once("open", () => {
