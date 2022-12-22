875bc0fade904d9079a1da00d5634ba62ddcbab5
clacson95 <caleb.lacson@gmail.com>
2022-12-21 19:10:00 -0500

    finished, testing

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userController");
+
+
+// /api/user
+router.route("/").get(getUsers).post(createUser);
+
+
+// /api/user/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+
+// /api/user/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+
+module.exports = router;
